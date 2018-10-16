a8c484832d7ea27a4b06a28de30efad9cdc7ad81
wutian <wutian@zafh.com.cn>
2018-10-16 19:18:54 +0800

    杭电ACM练习 1108 最小公倍数 by WuTian 2018-Oct-16

---
+++ src/main/java/com/wt/acm/hdu/page2/Problem1108.java
@@ -0,0 +1,29 @@
+package com.wt.acm.hdu.page2;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * @author WuTian
+ * @date 2018-10-16 19:07
+ * @description 最小公倍数
+ */
+public class Problem1108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new BufferedInputStream(System.in));
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a * b / gcd(a, b));
+        }
+    }
+
+    private static int gcd(int a, int b) {
+        while (a % b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
