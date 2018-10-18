d07b0ba3fcc3c651ec1a6b24ad0b23946d319a17
wutian <wutian@zafh.com.cn>
2018-10-18 18:03:10 +0800

    杭电ACM练习 1229 还是A+B by WuTian 2018-Oct-18

---
+++ src/main/java/com/wt/acm/hdu/page3/Problem1229.java
@@ -0,0 +1,48 @@
+package com.wt.acm.hdu.page3;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * @author WuTian
+ * @date 2018-10-18 17:34
+ * @description 还是A+B
+ */
+public class Problem1229 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new BufferedInputStream(System.in));
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int k = sc.nextInt();
+            if (a == 0 && b == 0)
+                break;
+            if (equal(a, b, k))
+                System.out.println(-1);
+            else
+                System.out.println(a + b);
+        }
+    }
+
+    private static boolean equal(int a, int b, int k) {
+        String strA = a + "";
+        String strB = b + "";
+        String subA = getTail(strA,k);
+        String subB = getTail(strB,k);
+        return subA.equals(subB);
+    }
+
+    private static String getTail(String input, int k) {
+        String result;
+        if (input.length() < k) {
+            result = input;
+            for (int i = 0; i < (k - input.length()); i++) {
+                result = "0" + result;
+            }
+        } else {
+            result = input.substring(input.length() - k + 1);
+        }
+        return result;
+    }
+
+}
